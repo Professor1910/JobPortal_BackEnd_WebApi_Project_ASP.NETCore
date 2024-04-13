17ef3b26de42852d14de2450e833c9511a9dee41
Md Aziz <mohammedazizoctober.2001@gmail.com>
2024-04-13 09:40:19 +0530

    added Program.cs

---
+++ JobPortalApplication/JobPortalApplication/Program.cs
@@ -0,0 +1,35 @@
+using JobPortalApplication.Models;
+using JobPortalApplication.Models.DataManager;
+using JobPortalApplication.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ProjectContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ProjectCon")));
+        
+builder.Services.AddScoped<IDataJobProvider<JobProvider>,JobProviderManager>();
+builder.Services.AddScoped<IDataJobSeeker<JobSeeker>,JobSeekerManager>();
+builder.Services.AddScoped<IDataJob<Jobs>, JobManager>();
+builder.Services.AddScoped<IDataJobApply<JobApply>, JobApplyManager>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
